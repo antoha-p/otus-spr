f269bdcde42ad22430ae99e53e8a160d9683a971
Anton P <antonp1961@gmail.com>
2023-10-15 17:11:10 +0700

    init

---
+++ src/Infrastructure/Infrastructure.EntityFramework/Migrations/20211017122524_Initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.EntityFramework.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(nullable: true),
+                    CourseId = table.Column<int>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lessons_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_CourseId",
+                table: "Lessons",
+                column: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
---
+++ src/Infrastructure/Infrastructure.Repositories.Implementations/CourseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Services.Repositories.Abstractions;
+using Domain.Entities;
+using Infrastructure.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    /// <summary>
+    /// Репозиторий работы с курсами
+    /// </summary>
+    public class CourseRepository: Repository<Course, int>, ICourseRepository 
+    {
+        public CourseRepository(DatabaseContext context): base(context)
+        {
+        }
+      
+        /// <summary>
+        /// Получить постраничный список
+        /// </summary>
+        /// <param name="page">номер страницы</param>
+        /// <param name="itemsPerPage">объем страницы</param>
+        /// <returns> Список курсов</returns>
+        public async Task<List<Course>> GetPagedAsync(int page, int itemsPerPage)
+        {
+            var query = GetAll();
+            return await query
+                .Skip((page - 1) * itemsPerPage)
+                .Take(itemsPerPage)
+                .ToListAsync();
+        }
+    }
+}
